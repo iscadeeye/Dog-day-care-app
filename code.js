dfd91d6961b0276dcc93ed5891255f211cbb4208
mohamed hassan <hdeeq414@gmail.com>
2021-09-13 23:49:54 -0500

    added comments to clarify what each piece is doing and erased extra comments

--- code.js
+++ code.js
@@ -1,20 +1,14 @@
-// Follow the Instructions on my.kenzie.academy for this assignment.
-// Those instructions will give you details on each step.
-// STEP ONE - Create your Data Model.
+
 let dataModel = [
   { name: 'Teddy', breed: 'Akita', age: 5, likesTreats: true },
   { name: 'Milo', breed: 'Auggies', age: 3, likesTreats: true },
   { name: 'Max', breed: 'akbash', age: 4, likesTreats: false }
 ]
 
-// Assign your data model here, instead of null. This should be an array of "dog" objects.
-
-// When this function is run, it is meant to use the user input to build
-// a dog object, and add the dog object to the data model array.
 function onSubmitDog (event) {
   event.preventDefault()
 
-  let nameInput = document.querySelector('#name_input') // We provide a CSS selector, as a string, to identify which HTML element we want querySelector to find for us.
+  let nameInput = document.querySelector('#name_input')
   let breedInput = document.querySelector('#breed_input')
   let ageInput = document.querySelector('#age_input')
   let treatsCheckbox = document.querySelector('#treats_input')
@@ -29,11 +23,6 @@ function onSubmitDog (event) {
     alert('Please fill out all of the fields!')
     return // Exit the function early if the above condition is true.
   }
-  // STEP TWO - Create a "dog" variable. What piece of data will we assign
-  // to this variable? A new "dog" object, containing the values from above:
-  // name, breed, age, likeTreats. Add this object to your data model array.
-  // How can you insert this dog object into the dogs array?
-  // YOUR CODE HERE
 
   let dog = {
     name,
@@ -42,12 +31,10 @@ function onSubmitDog (event) {
     likesTreats
   }
   dataModel.push(dog)
-  // worked  with Michael Meacham (Facilitator). learned some cool stuff from him. really appreciated.
 
   renderDogList()
 
-  // Now that we have added a new dog to the data model,
-  // we should render the dog list on the page again.
+  
 
   // The following lines reset the form, so that it is ready for information
   // on a new dog:
@@ -57,8 +44,8 @@ function onSubmitDog (event) {
   treatsCheckbox.checked = false
 }
 
-// This function is run, it is meant to keep the dog list which the user
-// sees on the page in sync with the data model containing all of our
+// when this function is run, it is meant to keep the dog list which the user
+// sees on the page in sync with the data model containing all
 // dog objects.
 
 function renderDogList () {
@@ -69,8 +56,8 @@ function renderDogList () {
     let li = document.createElement('li')
     li.innerHTML = 'No Dogs!'
     list.append(li)
-    
-    return //exit the function early
+
+    return //exit the function early this the above is true
   }
   for (let index = 0; index < dataModel.length; index += 1) {
     let dog = dataModel[index]
@@ -83,11 +70,9 @@ function renderDogList () {
       removeDog(dog)
     }
 
-    // STEP THREE - Render the dog list from scratch. See "Step Three"
-    // instructions.
     // If there are no dogs, then render "No Dogs!" Otherwise, render all
     // of the dogs in your data model.
-    
+
     if (dataModel[index].likesTreats === true) {
       li.innerHTML = `${dataModel[index].name}! A ${dataModel[index].age} year old ${dataModel[index].breed}, which likes treats  `
       li.appendChild(sendHomeButton), list.append(li)
@@ -105,7 +90,6 @@ function removeDog (dog) {
   renderDogList()
 }
 
-
 // Run the onSubmitDog function when the button is clicked.
 let button = document.querySelector('#submit_button')
 button.addEventListener('click', onSubmitDog)
@@ -113,3 +97,4 @@ button.addEventListener('click', onSubmitDog)
 // This function call will take place when the page loads, in order
 // to render the dog list for the very first time.
 renderDogList()
+
